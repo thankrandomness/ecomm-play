9da562cc6602449eb3d062e162ba93865295b51f
thankrandomness <vijaybchakilam@gmail.com>
2021-06-15 14:16:22 -0400

    test code pipeline self mutate change

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'Hello from a Lambda Function',
+    body: 'Hello world from a Lambda Function',
     statusCode: 200,
   };
 }
